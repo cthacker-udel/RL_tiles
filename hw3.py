ab3cc0dd3f099352e0c786b6f084c6bb59068d44
cthacker-udel <cthacker@udel.edu>
2023-10-25 12:10:46 -0400

    feat: added setup

--- hw3.py
+++ hw3.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from enum import Enum
 from typing import Optional
+import random
+random.seed(1)
 
 """
 [ ] - Implement basic board functionality (placing tiles where they need to be)
@@ -24,6 +26,12 @@ test_case_7 = ["13 11 16 5 p", "1\tright\n2\tup\n3\tup\n4\tup\n5\twall-square\n6
 test_case_8 = ["13 11 7 15 p", "1\tup\n2\tup\n3\tright\n4\tup\n5\tup\n6\tup\n7\tforbid\n8\tup\n9\tup\n10\tright\n11\tgoal\n12\tleft\n13\tgoal\n14\tleft\n15\twall-square\n16\tdown\n"]
 test_cases = [test_case_1, test_case_2, test_case_3, test_case_4,
               test_case_5, test_case_6, test_case_7, test_case_8]
+
+# endregion
+
+# region Constants
+
+START_IND = 2
 
 # endregion
 
@@ -55,23 +63,27 @@ class AgentAction(Enum):
 
 
 class BoardTile:
-    def __init__(self: BoardTile, index: int = -1, tile_type=TileType.NORMAL) -> None:
+    def __init__(self: BoardTile, x: int, y: int, index: int = -1, tile_type=TileType.NORMAL) -> None:
+        self.x = x
+        self.y = y
         self.index: int = index
         self.tile_type: TileType = tile_type
         self.q_north = 0  # up
         self.q_east = 0  # right
         self.q_south = 0  # down
         self.q_west = 0  # left
+        self.reward = 0  # reward for tile
 
 
 class ParsedInput:
-    def __init__(self: ParsedInput, goal_1_ind: int, goal_2_ind: int, forbid_ind: int, wall_ind: int, output_format: str, q_ind: Optional[int] = None):
+    def __init__(self: ParsedInput, start_ind: int, goal_1_ind: int, goal_2_ind: int, forbid_ind: int, wall_ind: int, output_format: str, q_ind: Optional[int] = None):
         self.goal_1_ind = goal_1_ind
         self.goal_2_ind = goal_2_ind
         self.forbid_ind = forbid_ind
         self.wall_ind = wall_ind
         self.output_format: OutputFormat = OutputFormat.PRINT if output_format == "p" else OutputFormat.OPTIMAL_Q
         self.q_ind = q_ind
+        self.start_ind = start_ind
 
     def classify_tile_by_ind(self: ParsedInput, ind: int) -> TileType:
         if ind == self.goal_1_ind or ind == self.goal_2_ind:
@@ -80,11 +92,31 @@ class ParsedInput:
             return TileType.FORBIDDEN
         elif ind == self.wall_ind:
             return TileType.WALL
+        elif ind == self.start_ind:
+            return TileType.START
         else:
             return TileType.NORMAL
 
-    def __str__(self: ParsedInput):
-        return f'{self.goal_1_ind} {self.goal_2_ind} {self.forbid_ind} {self.wall_ind} {"p" if self.output_format == OutputFormat.PRINT else "q"}{("" + str(self.q_ind)) if self.q_ind is not None else ""}'
+
+class Agent:
+    def __init__(self: Agent, x=0, y=0, ind=0):
+        self.x = x
+        self.y = y
+        self.ind = ind
+        self.moves = [AgentAction.UP, AgentAction.DOWN,
+                      AgentAction.LEFT, AgentAction.RIGHT]
+
+    def choose_move(self: Agent, policy: AgentAction, epsilon=1):
+        rand_value = random.random()
+        curr_policy = 1 - epsilon
+        if rand_value <= curr_policy:
+            # choose to move randomly
+            return policy
+
+        return random.choice(self.moves)
+
+    def clone(self: Agent):
+        return Agent(self.x, self.y, self.ind)
 
 # endregion
 
@@ -96,7 +128,7 @@ def create_basic_board(rows, cols) -> list[list[BoardTile]]:
     for i in range(rows):
         sub_row = []
         for j in range(cols):
-            sub_row.append(BoardTile((i + 1) + j))
+            sub_row.append(BoardTile(j, i, (i + 1) + j))
         board.append(sub_row)
     return board
 
@@ -107,6 +139,10 @@ def apply_input_to_board(board: list[list[BoardTile]], parsed_input: ParsedInput
             tile_classification = parsed_input.classify_tile_by_ind(
                 each_tile.index)
             each_tile.tile_type = tile_classification
+            if tile_classification == TileType.GOAL:
+                each_tile.reward = 100
+            elif tile_classification == TileType.FORBIDDEN:
+                each_tile.reward = -100
     return board
 
 
@@ -114,19 +150,44 @@ def parse_input(inp: str) -> ParsedInput:
     split_inp = inp.split(' ')
     if len(split_inp) == 5:
         [goal_ind1, goal_ind2, forbidden_ind, wall_ind, output_type] = split_inp
-        return ParsedInput(int(goal_ind1), int(goal_ind2), int(forbidden_ind), int(wall_ind), output_type)
+        return ParsedInput(START_IND, int(goal_ind1), int(goal_ind2), int(forbidden_ind), int(wall_ind), output_type)
     else:
         [goal_ind1, goal_ind2, forbidden_ind, wall_ind,
             output_type, q_value_ind] = split_inp
-        return ParsedInput(int(goal_ind1), int(goal_ind2), int(forbidden_ind), int(wall_ind), output_type, int(q_value_ind))
+        return ParsedInput(START_IND, int(goal_ind1), int(goal_ind2), int(forbidden_ind), int(wall_ind), output_type, int(q_value_ind))
+
+
+def find_tile_by_ind(board: list[list[BoardTile]], ind: int) -> BoardTile:
+    for each_row in board:
+        for each_col in each_row:
+            if each_col.index == ind:
+                return each_col
+    raise Exception("Tile index does not exist")
+
+
 # endregion
 
 # region Main Classes
 
 
 class BoardSolver:
-    def __init__(self: BoardSolver, rows=4, cols=4,):
-        self.board = create_basic_board(rows, cols)
+    def __init__(self: BoardSolver, rows=4, cols=4, living_reward=-0.1, discount=0.1, learning_rate=0.3, max_iter=100_000, board: Optional[list[list[BoardTile]]] = None, agent: Optional[Agent] = None):
+        self.board: list[list[BoardTile]] = board if board is not None else create_basic_board(
+            rows, cols)
+        self.living_reward = living_reward  # r
+        self.discount_rate = discount  # gamma
+        self.learning_rate = learning_rate  # alpha
+        self.iterations = 0
+        self.max_iter = max_iter
+        if self.agent is None:
+            found_start_tile = find_tile_by_ind(self.board, START_IND)
+            self.agent = Agent(found_start_tile.x, found_start_tile.y)
+        else:
+            self.agent = agent
+
+    def learn(self: BoardSolver) -> None:
+        pass
+
 
 # endregion
 
